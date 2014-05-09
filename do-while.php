a44fb79fa43685c1ce34d18cc8f166823a4e84e3
Andre Dempsey <andre.dempsey@live.com>
2014-05-09 15:56:19 -0500

    Created do-while file; starts at 0 increments by 2, stops at 100

---
+++ do-while.php
@@ -0,0 +1,21 @@
+<?php
+
+//This is the exercise for 
+//Control Structures I
+//Page: 5 of 5
+//Date:  9 May 14
+//Name:  Andre Dempsey
+//Codeup Baddies
+
+//initialize $number
+
+$number = 0;
+
+do 
+{
+	echo $number . PHP_EOL;
+	$number +=2;
+}
+while ($number <= 100) 
+
+?>
