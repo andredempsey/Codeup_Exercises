da1de07b35b61cd8ee848cb82d398f35d6e0520f
Andre Dempsey <andre.dempsey@live.com>
2014-05-09 15:10:07 -0500

    First draft of while code

---
+++ while.php
@@ -0,0 +1,21 @@
+<?php
+
+//This is the exercise for 
+//Control Structures I
+//Page: 4 of 5
+//Date:  9 May 14
+//Name:  Andre Dempsey
+//Codeup Baddies
+
+
+//initialize $test
+
+$test = 5;
+while ($test <= 15) 
+{
+    echo "{$test}\n";
+    $test++;
+}
+
+
+?>
