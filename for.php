b96158240efe2a58b5257590419032549b3fe945
Andre Dempsey <andre.dempsey@live.com>
2014-05-12 20:38:33 -0500

    Default incrementer to 1 if no input

--- for.php
+++ for.php
@@ -25,6 +25,9 @@ fwrite(STDOUT, 'Please enter the incrementer for the series => ');
 // Get the input from user
 $incrementer=(int)fgets(STDIN);
 
+//default incrementer to 1 if no value or invalid value
+$incrementer = $incrementer != 0 ? $incrementer : 1;
+
 //loop through the numbers
 for ($i = $start; $i <= $end; $i+=$incrementer) 
 { 
