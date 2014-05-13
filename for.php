ba3656349064549723f8f1504cedc7370d6d485b
Andre Dempsey <andre.dempsey@live.com>
2014-05-13 11:20:31 -0500

    Fixed problem with defaulting incrementer to 1

--- for.php
+++ for.php
@@ -11,7 +11,7 @@ if ($argc>=5 && $argv[1]=="-cl") //verify there are sufficient arguments AND cor
 {
 	$start = CheckInput($argv[2], True, $argv[0]);
 	$end = CheckInput($argv[3], True, $argv[0]);
-	$incrementer = CheckInput($argv[4], True, $argv[0]);
+	$incrementer = CheckIncrementer($argv[4], True, $argv[0]);
 }
 else //no valid command line arguments
 {
@@ -19,23 +19,23 @@ else //no valid command line arguments
 	fwrite(STDOUT, 'Please enter a starting number for the series => ');
 	  
 	// Get the input from user; check for numeric
-	$start=checkinput(trim(fgets(STDIN)),False, $argv[0]);
+	$start=CheckInput(trim(fgets(STDIN)),False, $argv[0]);
 
 	// Prompt user for ending number
 	fwrite(STDOUT, 'Please enter the ending number for the series => ');
 	  
 	// Get the input from user
-	$end=checkinput(trim(fgets(STDIN)),False, $argv[0]);
+	$end=CheckInput(trim(fgets(STDIN)),False, $argv[0]);
 
 	// Prompt user for incrementer
 	fwrite(STDOUT, 'Please enter the incrementer for the series => ');
 	  
 	// Get the input from user
-	$incrementer=checkinput(trim(fgets(STDIN)), False,$argv[0]);
+	$incrementer=CheckIncrementer(trim(fgets(STDIN)), False, $argv[0]);
+	
+	//default incrementer to 1 if no value or invalid value
+	//$incrementer = $incrementer != 0 ? $incrementer : 1;
 }
-
-//default incrementer to 1 if no value or invalid value
-$incrementer = $incrementer != 0 ? $incrementer : 1;
 
 //loop through the numbers
 for ($i = $start; $i <= $end; $i+=$incrementer) 
@@ -52,6 +52,7 @@ function CheckInput($arg, $commandline, $file)
 {
 	if (is_numeric($arg)) 
 	{
+	
 		return (int) $arg;
 	} 
 	elseif ($commandline) 
@@ -63,5 +64,31 @@ function CheckInput($arg, $commandline, $file)
 	{
 		echo "This value must be numeric.\n";
 		exit(1);
+	}
+}
+
+function CheckIncrementer($arg, $commandline, $file)  
+
+//$arg is value to be checked
+//$commandline is boolean to see if input is coming from command line
+//$file is the file name; only used for Usage message
+
+{
+	if (is_numeric($arg)) 
+	{
+		//default incrementer to 1 if no value or invalid value
+		$arg = $arg != 0 ? $arg : 1;
+		return (int) $arg;
+	} 
+	elseif ($commandline) 
+	{
+		echo "Usage:  With command line arguments => \n php {$file} -cl [start as integer] [end as integer] [incrementer as integer] \n\n	Without command line arguments => \n php {$file}\n";
+		exit(1);
+	}
+	else 
+	{
+		//default incrementer to 1 if no value or invalid value
+		$arg = $arg != 0 ? $arg : 1;
+		return (int) $arg;
 	}
 }
