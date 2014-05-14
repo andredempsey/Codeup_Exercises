fc555b13efc2362f54a33c796b6e3a02c24880ad
Andre Dempsey <andre.dempsey@live.com>
2014-05-13 19:11:34 -0500

    iterate array using for loop

---
+++ fruit.php
@@ -0,0 +1,15 @@
+<?php
+
+//Fruits Assignment 
+//iterate array using for loop
+//Date:  13 May 14
+//Name:  Andre Dempsey
+//Codeup Baddies
+
+
+$fruits = ["apple", "banana", "orange", "pear", "kiwi", "grapes"];
+
+for ($i=0;$i<count($fruits); $i++) 
+{
+    echo "{$fruits[$i]}\n";    
+}
