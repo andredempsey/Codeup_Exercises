0dc35fd003a691051bab3933ee42770d7cea8564
Andre Dempsey <andre.dempsey@live.com>
2014-05-14 11:58:04 -0500

    create switch to output day of week based on todays date

---
+++ switch.php
@@ -0,0 +1,40 @@
+<?php
+//This is the switch exercise for 
+//Control Structures II
+//Page: 5 of 5
+//Date:  14 May 14
+//Name:  Andre Dempsey
+//Codeup Baddies
+
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');
+
+switch($day_of_week) {
+    case 1:
+        echo "Monday\n";
+        break;
+    case 2:
+        echo "Tuesday\n";
+        break;
+    case 3:
+        echo "Wednesday\n";
+        break;
+    case 4:
+        echo "Thursday\n";
+        break;
+    case 5:
+        echo "Friday\n";
+        break;
+    case 6:
+        echo "Saturday\n";
+        break;
+    case 7:
+        echo "Sunday\n";
+        break;
+    
+}
