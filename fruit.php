2f5c98fdeb29c46b6372495dd1c6b57f0330f43e
Andre Dempsey <andre.dempsey@live.com>
2014-05-14 09:28:12 -0500

    revised array from multidimensional to single dimensional and updated solution

--- fruit.php
+++ fruit.php
@@ -7,16 +7,32 @@
 //Codeup Baddies
 
 
-$fruits = [
-    ["type"=>"Apples","color"=>"red"],
-    ["type"=>"Bananas","color"=>"yellow"],
-    ["type"=>"Oranges","color"=>"orange"],
-    ["type"=>"Pears", "color"=>"green"],
-    ["type"=>"Kiwis", "color"=>"green"],
-    ["type"=>"Grapes","color"=>"purple"]];
+// $fruits = [
+//     ["type"=>"Apples","color"=>"red"],
+//     ["type"=>"Bananas","color"=>"yellow"],
+//     ["type"=>"Oranges","color"=>"orange"],
+//     ["type"=>"Pears", "color"=>"green"],
+//     ["type"=>"Kiwis", "color"=>"green"],
+//     ["type"=>"Grapes","color"=>"purple"]];
 
-foreach ($fruits as $key =>$fruit) 
+// foreach ($fruits as $key =>$fruit) 
+// {
+//         echo "{$fruit['type']} are {$fruit['color']}.\n";
+
+// }
+
+
+//adjusted to match Jason's solution
+$fruits = [
+    "Apples"=>"red",
+    "Bananas"=>"yellow",
+    "Oranges"=>"orange",
+    "Pears"=>"green",
+    "Kiwis"=>"green",
+    "Grapes"=>"purple"];
+
+foreach ($fruits as $fruit =>$color) 
 {
-        echo "{$fruit['type']} are {$fruit['color']}.\n";
+        echo "{$fruit} are {$color}.\n";
 
 }
