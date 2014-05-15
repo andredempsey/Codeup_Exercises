84e7c80407203394e8f38dff9b4f3403c757cf51
Andre Dempsey <andre.dempsey@live.com>
2014-05-15 09:46:52 -0500

    initial load of functions with test code

---
+++ arithmetic.php
@@ -0,0 +1,36 @@
+<?php
+
+//This is the exercise for 
+//Functions I
+//Page: 1 of 5
+//Date:  15 May 14
+//Name:  Andre Dempsey
+//Codeup Baddies
+
+function add($a, $b) 
+{
+    echo $a + $b. PHP_EOL;
+}
+
+function subtract($a, $b) 
+{
+ 	echo $a - $b. PHP_EOL;
+}
+
+function multiply($a, $b) 
+{
+    echo $a * $b. PHP_EOL;
+}
+
+function divide($a, $b) 
+{
+    echo $a / $b. PHP_EOL;
+}
+
+//test the functions
+
+add(20,2);
+subtract(20,2);
+multiply(20,2);
+divide(20,2);
+
