4b1a4ea35a904cc3e1998955049f05458d5de1ab
Andre Dempsey <andre.dempsey@live.com>
2014-05-15 11:40:38 -0500

    Validate all the arguments, and display an error if the input is not numeric

--- arithmetic.php
+++ arithmetic.php
@@ -2,39 +2,86 @@
 
 //This is the exercise for 
 //Functions I
-//Page: 1 of 5
+//Page: 2 of 5
 //Date:  15 May 14
 //Name:  Andre Dempsey
 //Codeup Baddies
 
+//1. Validate all the arguments, and display an error if the input is not numeric.
+
 function add($a, $b) 
 {
-    echo $a + $b. PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
+	{
+    	echo $a + $b;
+    	echo PHP_EOL;
+ 	} 
+ 	else
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo PHP_EOL;	
+ 	}
 }
 
 function subtract($a, $b) 
 {
- 	echo $a - $b. PHP_EOL;
+if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
+	{
+    	echo $a - $b;
+    	echo PHP_EOL;
+ 	} 
+ 	else
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo PHP_EOL;	
+ 	}
 }
 
 function multiply($a, $b) 
 {
-    echo $a * $b. PHP_EOL;
+ if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
+	{
+    	echo $a * $b;
+    	echo PHP_EOL;
+ 	} 
+ 	else
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo PHP_EOL;	
+ 	}
 }
 
 function divide($a, $b) 
 {
-    echo $a / $b. PHP_EOL;
+ if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
+	{
+    	echo $a / $b;
+    	echo PHP_EOL;
+ 	} 
+ 	else
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo PHP_EOL;	
+ 	}
 }
 
 function modulus($a, $b) 
 {
-    echo $a % $b. PHP_EOL;
+ if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
+	{
+    	echo $a % $b;
+    	echo PHP_EOL;
+ 	} 
+ 	else
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo PHP_EOL;	
+ 	}
 }
 
 //test the functions
 
-add(20,2);
+add('test',2);
 subtract(20,2);
 multiply(20,2);
 divide(20,2);
