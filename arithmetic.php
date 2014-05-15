18fb2a4b6421093d6b9388ec540f91bb447c410c
Andre Dempsey <andre.dempsey@live.com>
2014-05-15 13:21:34 -0500

    Make the error messages show the values of the arguments.

--- arithmetic.php
+++ arithmetic.php
@@ -9,6 +9,7 @@
 
 //1. Validate all the arguments, and display an error if the input is not numeric.
 //2. Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+//3.  Make the error messages show the values of the arguments.
 
 function add($a, $b) 
 {
@@ -18,7 +19,26 @@ function add($a, $b)
  	} 
  	else
  	{
- 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo "ERROR:  Please limit your inputs to numeric values. ";
+ 		echo PHP_EOL;	
+ 		if (is_array($a)) 
+ 		{
+ 			echo "\$a is an array.\n";	
+ 			print_r($a);
+ 		}
+ 		elseif(!is_numeric($a))
+ 		{
+ 			echo "The value of \$a is {$a} and it is NOT numeric.\n";
+ 		}	
+ 		if (is_array($b)) 
+ 		{
+ 			echo "\$b is an array.\n";	
+ 			print_r($b);
+ 		}
+ 		elseif(!is_numeric($b))
+ 		{
+ 			echo "The value of \$b is {$b} and it is NOT numeric.\n";
+ 		}	
  	}
  	echo PHP_EOL;	
 }
@@ -31,7 +51,26 @@ if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
     } 
  	else
  	{
- 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo "ERROR:  Please limit your inputs to numeric values. ";
+ 		echo PHP_EOL;	
+ 		if (is_array($a)) 
+ 		{
+ 			echo "\$a is an array.\n";	
+ 			print_r($a);
+ 		}
+ 		elseif(!is_numeric($a))
+ 		{
+ 			echo "The value of \$a is {$a} and it is NOT numeric.\n";
+ 		}	
+ 		if (is_array($b)) 
+ 		{
+ 			echo "\$b is an array.\n";	
+ 			print_r($b);
+ 		}
+ 		elseif(!is_numeric($b))
+ 		{
+ 			echo "The value of \$b is {$b} and it is NOT numeric.\n";
+ 		}	
  	}
  	echo PHP_EOL;	
 }
@@ -44,55 +83,112 @@ function multiply($a, $b)
  	} 
  	else
  	{
- 		echo "ERROR:  Please limit your inputs to numeric values.";
+ 		echo "ERROR:  Please limit your inputs to numeric values. ";
+ 		echo PHP_EOL;	
+ 		if (is_array($a)) 
+ 		{
+ 			echo "\$a is an array.\n";	
+ 			print_r($a);
+ 		}
+ 		elseif(!is_numeric($a))
+ 		{
+ 			echo "The value of \$a is {$a} and it is NOT numeric.\n";
+ 		}	
+ 		if (is_array($b)) 
+ 		{
+ 			echo "\$b is an array.\n";	
+ 			print_r($b);
+ 		}
+ 		elseif(!is_numeric($b))
+ 		{
+ 			echo "The value of \$b is {$b} and it is NOT numeric.\n";
+ 		}	
  	}
  	echo PHP_EOL;	
 }
 
 function divide($a, $b) 
 {
+	if ($b==0) //check for zero value before dividing to trap error
+  	{
+  		echo "ERROR:  You cannot enter a $b for the divisor.\n";		
+    } 
  if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
  	{
     	if ($b!=0) //check for zero value before dividing to trap error
     	{
     		echo $a / $b;
     	}
-    	else
-    	{
-    		echo "ERROR:  You cannot enter a zero for the divisor.";		
-    	} 
     }
     else
-    {
-    	echo "ERROR:  Please limit your inputs to numeric values.";
-    }
-    echo PHP_EOL;	
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values. ";
+ 		echo PHP_EOL;	
+ 		if (is_array($a)) 
+ 		{
+ 			echo "\$a is an array.\n";	
+ 			print_r($a);
+ 		}
+ 		elseif(!is_numeric($a))
+ 		{
+ 			echo "The value of \$a is {$a} and it is NOT numeric.\n";
+ 		}	
+ 		if (is_array($b)) 
+ 		{
+ 			echo "\$b is an array.\n";	
+ 			print_r($b);
+ 		}
+ 		elseif(!is_numeric($b))
+ 		{
+ 			echo "The value of \$b is {$b} and it is NOT numeric.\n";
+ 		}	
+ 	}
+ 	echo PHP_EOL;	
 }
 function modulus($a, $b) 
 {
+	if ($b==0) //check for zero value before dividing to trap error
+  	{
+  		echo "ERROR:  You cannot enter a $b for the divisor.\n";		
+    } 
  	if (is_numeric($a) && is_numeric($b)) //check to ensure variables are numeric
  	{
     	if ($b!=0) //check for zero value before dividing to trap error
     	{
     		echo $a % $b;
-    	}
-    	else
-    	{
-    		echo "ERROR:  You cannot enter a zero for the divisor.";		
     	} 
     }
     else
-    {
-    	echo "ERROR:  Please limit your inputs to numeric values.";
-    }
-    echo PHP_EOL;	
+ 	{
+ 		echo "ERROR:  Please limit your inputs to numeric values. ";
+ 		echo PHP_EOL;	
+ 		if (is_array($a)) 
+ 		{
+ 			echo "\$a is an array.\n";	
+ 			print_r($a);
+ 		}
+ 		elseif(!is_numeric($a))
+ 		{
+ 			echo "The value of \$a is {$a} and it is NOT numeric.\n";
+ 		}	
+ 		if (is_array($b)) 
+ 		{
+ 			echo "\$b is an array.\n";	
+ 			print_r($b);
+ 		}
+ 		elseif(!is_numeric($b))
+ 		{
+ 			echo "The value of \$b is {$b} and it is NOT numeric.\n";
+ 		}	
+ 	}
+ 	echo PHP_EOL;	
 }
 
 //test the functions
 
-add('test',2);
-subtract(20,2);
-multiply(20,2);
-divide(20,0);
-modulus(1,0);
+// add('test',2);
+//modulus(["pizza","hamburgers"],1);
+// multiply(20,2);
+// divide(20,0);
+// modulus(1,2);
 
