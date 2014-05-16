9f809fb8650facd2c032bbb3e217767d4baf2cc5
Andre Dempsey <andre.dempsey@live.com>
2014-05-16 11:00:36 -0500

    Before the first conditional, unset()

--- internal_functions.php
+++ internal_functions.php
@@ -10,6 +10,8 @@
 
 //1. Create the program to meet the criteria set by each comment.
 //2. Test and verify the output is as expected.
+//3. Update the 'is set' check on $something to see if it is 'empty'. What happens?
+//4. Before the first conditional, unset($nothing). What happens?
 
 
 $nothing = NULL;
@@ -24,30 +26,36 @@ function check_var($variable_name)
 	{
 		echo "variable_name is SET.\n";
 	}
-	elseif (empty($variable_name)) 
+	if (empty($variable_name)) 
 	{
 		echo "variable_name is EMPTY.\n";
 	}
 
 }
+unset($nothing);
 // TEST: If var $nothing is set, display '$nothing is SET'
+echo "TEST: If var \$nothing is set, display '\$nothing is SET'\n";
 $nothing = 'not empty';
 check_var($nothing);
 
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
+echo "TEST: If var \$nothing is empty, display '\$nothing is EMPTY'\n";
 $nothing = NULL;
 check_var($nothing);
 
 // TEST: If var $something is set, display '$something is SET'
+echo "TEST: If var \$something is set, display '\$something is SET'\n";
 
 check_var($something);
 
 // Serialize the array $array, and output the results
+echo 'Serialize the array $array, and output the results'.PHP_EOL;
 $serialized_array = serialize($array);
 echo $serialized_array;
 echo PHP_EOL;
 
 // Unserialize the array $array, and output the results
+echo 'Unserialize the array $array, and output the results'.PHP_EOL;
 echo "Array (" . implode(",",unserialize($serialized_array)) . ")";
 echo PHP_EOL;
 
